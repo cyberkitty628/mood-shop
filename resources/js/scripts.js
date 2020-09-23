ece51a9eb8191f40a12a71f1ef1122c00c1d461d
Kristina <cyberkitty628@gmail.com>
2020-09-22 19:41:21 -0700

    Displayed moods with button using Javascript

---
+++ resources/js/scripts.js
@@ -0,0 +1,43 @@
+const itemsContainer = document.getElementById('items')
+
+import data from './data.js'
+
+// the length of our data determines how many times this loop goes around
+for (let i=0; i<data.length; ++i) {
+    // create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    // create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    // Add the image to the div
+    newDiv.appendChild(img)
+    console.log(img)
+    // put new div inside items container
+    itemsContainer.appendChild(newDiv)
+
+    let desc = document.createElement('P')
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+    let button = document.createElement('button')
+    button.id = data[i].name
+
+    // creates a custom attribute called data-price. 
+    // That will hold price for each element in the button
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+    
+    // put new div inside items container
+    itemsContainer.appendChild(newDiv)
+}
